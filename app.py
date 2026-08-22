31ea1039776dbbc210b26b002a3d9331549349dc
TANJIP SURAIT MAHDIN <154902928+MaHdIn2020@users.noreply.github.com>
2026-08-22 19:36:13 +0600

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,11 @@ import csv
 from jobspy import scrape_jobs
 
 jobs = scrape_jobs(
-    site_name=["indeed", "linkedin", "bdjobs", "google"], # "glassdoor", "bayt", "naukri", 
-    search_term="Machine Learning Engineer",
-    google_search_term="software engineer jobs in dhaka, Bangladesh since this week",
-    location="Dhaka, Bangladesh",
+    site_name=["indeed", "linkedin", "google"],
+    search_term="machine learning engineer",
+    location="Dhaka",
     results_wanted=20,
-    hours_old=72,
-    country_indeed='Bangladesh',
-    
-    # linkedin_fetch_description=True # gets more info such as description, direct job url (slower)
-    # proxies=["208.195.175.46:65095", "208.195.175.45:65095", "localhost"],
+    hours_old=72
 )
+
 print(jobs.to_string())
